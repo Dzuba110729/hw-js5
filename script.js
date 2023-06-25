c83ec9b88b96413e68bfed05b3afd8a4844f148d
dzuba110729 <dzyuba1107@outlook.com>
2023-06-25 21:30:36 +0300

    homework

---
+++ script.js
@@ -0,0 +1,34 @@
+'use strict';
+// Задача 1
+// function buttonClick() {
+//	let elem = document.getElementById('elem');
+//	elem.innerHTML = 'Ку-ку! А я жирный!';
+// }
+
+// Задача 2
+// function buttonClick() {
+//	let elem = document.getElementById('elem');
+//	elem.innerHTML = 'Абзац превратился в h3!';
+//	elem.outerHTML = '<h3>' + elem.innerHTML + '</h3>';
+// }
+
+// Задча 3
+// function buttonClick() {
+//	let elem = document.getElementById('elem');
+//	elem.outerHTML = '<h3>' + elem.innerHTML + '</h3>';
+// }
+
+// Задача 4
+// function buttonClick() {
+//	let elem = document.getElementById('elem');
+//	let sum = document.forms[1] + document.forms[2];
+//	elem.innerHTML = sum;
+// }
+
+// Задача 5
+// function buttonClick() {
+//	let elems = document.querySelectorAll('.a');
+//	for (let i = 0; i < elems.length; i++) {
+//		elems[i].innerHTML = i + 1;
+//	}
+// }
